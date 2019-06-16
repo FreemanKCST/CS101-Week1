7965193da44f21de572abcdd8697e9e75114359b
FreemanKCST <51857528+FreemanKCST@users.noreply.github.com>
2019-06-16 13:13:49 +0300

    Add files via upload

--- getline.cpp
+++ getline.cpp
@@ -1,5 +1,3 @@
-// This program demonstrates using the getline function
-// to read character data into a string object.
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,13 +10,10 @@ int main()
 	cout << "Please enter your first and last name: ";
 	getline(cin, name);
 	cout << "Enter the city you live in: ";
-	Enter the city you live in: ";
 	cin >> city;
 
-	\\getline(cin, city);
-
+	//getline(cin, city);
 	cout << "Hello, " << name << endl;
 	cout << "You live in " << city << endl;
 	return 0;
 }
-
